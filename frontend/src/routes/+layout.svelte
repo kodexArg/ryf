40e76613f9daed1035cc294f47a6aab6c134ffb7
Kodex <gcavedal@gmail.com>
2023-08-21 01:24:52 -0300

    Tailwind rdy

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+  import "../app.css"
+</script>
+
+<slot class="w-full h-full"/>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: #f2f2f2;
+  }
+</style>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section>
+	<h1>Welcome to SvelteKit</h1>
+</section>
+
+<style>
+	section {
+		@apply w-screen h-screen;
+    @apply grid place-items-center;
+	}
+
+	h1 {
+		@apply text-3xl font-bold bg-sky-500;
+    @apply rounded-full px-2 py-1;
+	}
+</style>
